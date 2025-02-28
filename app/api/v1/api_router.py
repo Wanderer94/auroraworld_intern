1dc4e72de9b62e24e8142e415add41948057e377
JunwooPark <wnsdn1994@gmail.com>
2025-02-28 22:46:28 +0900

    chore: 웹 링크 API 라우터 통합

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import users, auth
+from app.api.v1.endpoints import users, auth, links
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+api_router.include_router(links.router, prefix="/links", tags=["Links"])
