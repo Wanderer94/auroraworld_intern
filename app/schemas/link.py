332e7f10f1357e77fc0ad7974135f06ef90aa47b
JunwooPark <wnsdn1994@gmail.com>
2025-02-28 22:40:57 +0900

    feat: 웹 링크 Pydantic 스키마 추가

---
+++ app/schemas/link.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, HttpUrl
+from datetime import datetime
+
+# 웹 링크 생성 요청 데이터 모델
+class LinkCreate(BaseModel):
+    name: str
+    url: HttpUrl
+    category: str
+
+# 웹 링크 응답 데이터 모델
+class LinkResponse(BaseModel):
+    id: int
+    name: str
+    url: str
+    category: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
